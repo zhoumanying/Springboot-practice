d0bca31ed6323bf7fe6e1e8fc0b9650233bbe23e
linxiaowei <linxiaowei@ut.cn>
2021-02-24 08:21:34 +0800

    feat:初始化项目

---
+++ src/main/java/com/exam/Test.java
@@ -0,0 +1,14 @@
+package com.exam;
+
+/**
+ * @author kaolvkaolv
+ * @date 2021/2/24
+ * @description 初始化项目
+ */
+public class Test {
+
+  public static void main(String[] args) {
+    System.out.println(12);
+  }
+
+}
